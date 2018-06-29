c049795995a0330702f1dc35caab5f8eb9bc0bfd
Fatma <fatmafarjalllah8@gmail.com>
2018-06-29 13:37:48 +0200

    Add files via upload

---
+++ test.ino
@@ -0,0 +1,43 @@
+int data;
+
+void setup() 
+{ 
+  Serial.begin(9600); 
+  pinMode(13, OUTPUT); 
+  digitalWrite (13, LOW); 
+  pinMode(12, OUTPUT); 
+  digitalWrite (12, LOW); 
+  pinMode(11, OUTPUT); 
+  digitalWrite (11, LOW); 
+   
+  Serial.println("This is my First Test.");
+}
+ 
+void loop() 
+{
+while (Serial.available())
+  {
+    data = Serial.read();
+  }
+
+  if (data == '1')
+  digitalWrite (13, HIGH);
+  delay(100);
+  digitalWrite (13, LOW);
+ 
+ 
+  if (data == '2')
+  digitalWrite (12, HIGH);
+  delay(100);
+  digitalWrite (12, LOW);
+  
+  if (data == '3')
+  digitalWrite (11, HIGH);
+  delay(100);
+  digitalWrite (11, LOW);
+
+if (data == '0')
+  digitalWrite (13, LOW); 
+  digitalWrite (12, LOW); 
+  digitalWrite (11, LOW); 
+}
---
+++ testpython.py
@@ -0,0 +1,249 @@
+i=1
+j=0
+r=0
+print("Python Online Test , Enter 0 to show Answer")
+print('  ')
+print ("Q1- Which of the following function convert a String to an object in python?")
+print("1- repr(x)")
+print("2- eval(str)")
+print("3- list(s)")
+while i==1:
+    i += 1
+    s = (int(raw_input("Enter your response: ")))
+    if (s==2):
+        j+=5
+    if (s==0):
+        print("Answer : 2")
+print('  ')
+
+print ("Q2- What is the following function returns the lowest index in list that obj appears?")
+print("1- list.index(obj)")
+print("2- list.insert(index, obj)")
+print("3- list.remove(obj)")
+while i==2:
+    i += 1
+    s = (int(raw_input("Enter your response: ")))
+    if (s==1):
+        j+=5
+    if (s==0):
+        print("Answer : 1")
+print('  ')
+print ("Q3- Which of the following function convert a string to a float in python?")
+print("1- int(x [,base])")
+print("2- str(x)")
+print("3- float(x)")
+while i==3:
+    i += 1
+    s = (int(raw_input("Enter your response: ")))
+    if (s==3):
+        j+=5
+    if (s==0):
+        print("Answer : 3")
+print('  ')
+print ("Q4- What is the following function gives the total length of the list?")
+print("1- cmp(list)")
+print("2- len(list) ")
+print("3- min(list)")
+while i==4:
+    i += 1
+    s = (int(raw_input("Enter your response: ")))
+    if (s==2):
+        j+=5
+    if (s==0):
+        print("Answer : 2")
+print('  ')
+print ("Q5- Which of the following data types is not supported in python?")
+print("1- Tuple")
+print("2- Dictionary")
+print("3- Generics")
+while i==5:
+    i += 1
+    s = (int(raw_input("Enter your response: ")))
+    if (s==3):
+        j+=5
+    if (s==0):
+        print("Answer : 3")
+print('  ')
+print ("Q6- Which of the following function merges elements in a sequence?")
+print("1- isupper()")
+print("2- join(seq)")
+print("3- len(string)")
+while i==6:
+    i += 1
+    s = (int(raw_input("Enter your response: ")))
+    if (s==2):
+        j+=5
+    if (s==0):
+        print("Answer : 2")
+print('  ')        
+print ("Q7-  Which of the following function convert a string to a long in python?")
+print("1- int(x[,base])")
+print("2- long(x[,base])")
+print("3- str(x)")
+while i==7:
+    i += 1
+    s = (int(raw_input("Enter your response: ")))
+    if (s==2):
+        j+=5
+    if (s==0):
+        print("Answer : 2")
+print('  ')
+print ("Q8- To configure your email address with Git , you use the command ?")
+print("1- gitconfig --user email")
+print("2- git config --global email")
+print("3- git config --global user.email")
+while i==8:
+    i += 1
+    s = (int(raw_input("Enter your response: ")))
+    if (s==3):
+        j+=5
+    if (s==0):
+        print("Answer : 3")
+print('  ')
+print ("Q9- What is the output of print str if str = 'Hello World!'?")
+print("1- Hello World!")
+print("2- Error")
+print("3- str")
+while i==9:
+    i += 1
+    s = (int(raw_input("Enter your response: ")))
+    if (s==1):
+        j+=5
+    if (s==0):
+        print("Answer : 1")
+print('  ')
+print ("Q10- What is the output of print tinytuple * 2 if tinytuple = (123, 'john')?")
+print("1- (123, 'john', 123, 'john')")
+print("2- (123, 'john') * 2")
+print("3- Error ")
+while i==10:
+    i += 1
+    s = (int(raw_input("Enter your response: ")))
+    if (s==1):
+        j+=5
+    if (s==0):
+        print("Answer : 1")
+print('  ')
+print ("Q11- What is the output of print tuple[2:] if tuple = ( 'abcd', 786 , 2.23, 'john', 70.2 )?")
+print("1- ('abcd', 786 , 2.23, 'john', 70.2 )")
+print("2- (786, 2.23)")
+print("3- (2.23, 'john', 70.2)")
+while i==11:
+    i += 1
+    s = (int(raw_input("Enter your response: ")))
+    if (s==3):
+        j+=5
+    if (s==0):
+        print("Answer : 3")
+print('  ')
+print ("Q12- What is the output of print str[2:5] if str = 'Hello World!'?")
+print("1- llo World!")
+print("2- H ")
+print("3- llo")
+while i==12:
+    i += 1
+    s = (int(raw_input("Enter your response: ")))
+    if (s==3):
+        j+=5
+    if (s==0):
+        print("Answer : 3")
+print('  ')
+print ("Q13- What is the output of [1, 2, 3] + [4, 5, 6]?")
+print("1- [1, 2, 3, 4, 5, 6]")
+print("2- [1, 2, 3],[4, 5, 6]")
+print("3- [5, 7,9]")
+while i==13:
+    i += 1
+    s = (int(raw_input("Enter your response: ")))
+    if (s==1):
+        j+=5
+    if (s==0):
+        print("Answer : 1")
+print('  ')
+print ("Q14- What is the output of print str[2:] if str = 'Hello World!'?")
+print("1- llo World!")
+print("2- H")
+print("3- llo")
+while i==14:
+    i += 1
+    s = (int(raw_input("Enter your response: ")))
+    if (s==1):
+        j+=5
+    if (s==0):
+        print("Answer : 1")
+print('  ')
+print ("Q15- To merge branch we use")
+print("1- git tag")
+print("2- git merge")
+print("3- git commit")
+while i==15:
+    i += 1
+    s = (int(raw_input("Enter your response: ")))
+    if (s==2):
+        j+=5
+    if (s==0):
+        print("Answer : 2")
+print('  ')
+print ("Q16- To add a remote branch we use")
+print("1- git remote add")
+print("2- git remote new")
+print("3- git branch remote add")
+while i==16:
+    i += 1
+    s = (int(raw_input("Enter your response: ")))
+    if (s==1):
+        j+=5
+    if (s==0):
+        print("Answer : 1")
+print('  ')
+print ("Q17- To delete a branch we use")
+print("1- git delete branch")
+print("2- git remove branch")
+print("3- git branch -d")
+while i==17:
+    i += 1
+    s = (int(raw_input("Enter your response: ")))
+    if (s==3):
+        j+=5
+    if (s==0):
+        print("Answer : 3")
+print('  ')
+print ("Q18- To clone a repo to local machine we use")
+print("1- git add clone")
+print("2- git clone")
+print("3- git duplicate")
+while i==18:
+    i += 1
+    s = (int(raw_input("Enter your response: ")))
+    if (s==2):
+        j+=5
+    if (s==0):
+        print("Answer : 2")
+print('  ')
+print ("Q19- To view the history of a repo in one line we use")
+print("1- git log --online")
+print("2- git log --line")
+print("3- git log --oneline")
+while i==19:
+    i += 1
+    s = (int(raw_input("Enter your response: ")))
+    if (s==3):
+        j+=5
+    if (s==0):
+        print("Answer : 3")
+print('  ')        
+print ("Q20- The name of the file we use to ignore files in Git")
+print("1- gitignore")
+print("2- git.ignore")
+print("3- .gitignore")
+while i==20:
+    i += 1
+    s = (int(raw_input("Enter your response: ")))
+    if (s==3):
+        j+=5
+    if (s==0):
+        print("Answer : 3")
+      
+r+=j
+print('  ')
+print "Result= " + str(r) + "%"
